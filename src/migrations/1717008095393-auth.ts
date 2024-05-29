d586a5e2af7316407a61c5ec5c2f59263894f100
Mykhailo Fomenko <85066667+MishaFomenko@users.noreply.github.com>
2024-05-29 15:12:50 -0400

    fixed pr template

--- src/migrations/1717008095393-auth.ts
+++ src/migrations/1717008095393-auth.ts
@@ -1,18 +1,25 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Auth1717008095393 implements MigrationInterface {
-    name = 'Auth1717008095393'
+  name = 'Auth1717008095393';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`test_entity\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_a1bc855479c8e37d6e96b9473a\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`user\` (\`id\` varchar(255) NOT NULL, \`name\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`test_entity\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_a1bc855479c8e37d6e96b9473a\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE \`user\` (\`id\` varchar(255) NOT NULL, \`name\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` ON \`user\``);
-        await queryRunner.query(`DROP TABLE \`user\``);
-        await queryRunner.query(`DROP INDEX \`IDX_a1bc855479c8e37d6e96b9473a\` ON \`test_entity\``);
-        await queryRunner.query(`DROP TABLE \`test_entity\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` ON \`user\``,
+    );
+    await queryRunner.query(`DROP TABLE \`user\``);
+    await queryRunner.query(
+      `DROP INDEX \`IDX_a1bc855479c8e37d6e96b9473a\` ON \`test_entity\``,
+    );
+    await queryRunner.query(`DROP TABLE \`test_entity\``);
+  }
 }
