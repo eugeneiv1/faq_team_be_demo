56f4e7d70224fdb8dccd97f6a3d46195113c39b1
Yevhenii <eugeneiv1@gmail.com>
2024-06-02 23:16:12 +0300

    refactor: google strategy

--- src/modules/google-auth/google.strategy.ts
+++ src/modules/google-auth/google.strategy.ts
@@ -20,7 +20,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
-    const { name, emails} = profile;
+    const { name, emails } = profile;
     const user = {
       email: emails[0].value,
       firstName: name.givenName,
