8f7352df9a7d8bd1244051d2dcda6fb7cefae05f
AlexandraVoloshyna <oleksandravolosyna@gmail.com>
2024-06-04 11:40:30 +0300

    feat: update sign-up method to use sendMail methods

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
+import cryptoRandomString from 'crypto-random-string';
 
+import { EMailTemplate } from '../mail/enums/mail-template.enum';
+import { ESubjectName } from '../mail/enums/subject-name.enum';
+import { MailService } from '../mail/mail.service';
 import { UserRepository } from '../repository/services/user.repository';
 import { UserService } from '../user/user.service';
 import { SignUpRequestDto } from './dto/request/sign-up.request.dto';
@@ -12,6 +16,7 @@ export class AuthService {
     private readonly userRepository: UserRepository,
     private readonly userService: UserService,
     private readonly configService: ConfigService,
+    private readonly mailService: MailService,
   ) {}
 
   public async signUp(dto: SignUpRequestDto): Promise<void> {
@@ -20,10 +25,21 @@ export class AuthService {
 
       const salt = +this.configService.get('SALT');
       const hashedPassword = await bcrypt.hash(dto.password, salt);
-
-      await this.userRepository.save(
-        this.userRepository.create({ ...dto, password: hashedPassword }),
-      );
+      const otp = cryptoRandomString({
+        length: this.configService.get('OTP_LENGTH'),
+        type: 'numeric',
+      });
+      await Promise.all([
+        await this.userRepository.save(
+          this.userRepository.create({ ...dto, password: hashedPassword }),
+        ),
+        await this.mailService.sendMail(
+          dto.email,
+          ESubjectName.VERIFY,
+          EMailTemplate.VERIFY,
+          { otp_code: otp, name: dto.user_name },
+        ),
+      ]);
     } catch (error) {
       if (
         error instanceof HttpException &&
