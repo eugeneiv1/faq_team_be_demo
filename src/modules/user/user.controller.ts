a3bf70099992b47c8f0e5b6d99b39119822650c0
AlexandraVoloshyna <oleksandravolosyna@gmail.com>
2024-06-04 11:01:49 +0300

    feat: add update user endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
 
 @ApiTags('User')
 @Controller('users')
 export class UserController {
-  constructor() {}
+  constructor(private readonly userService: UserService) {}
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<{ message: string }> {
+    await this.userService.updateUser(updateUserDto, id);
+    return { message: 'user updated successfully' };
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,18 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
 
 import { EErrorMessage } from '../../common/enums/error-message.enum';
 import { UserEntity } from '../../entities/user.entity';
 import { UserRepository } from '../repository/services/user.repository';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly configService: ConfigService,
+  ) {}
 
   public async isUserExist(userId: string): Promise<UserEntity> {
     try {
@@ -42,6 +48,37 @@ export class UserService {
           HttpStatus.BAD_REQUEST,
         );
       }
+    } catch (error) {
+      if (
+        error instanceof HttpException &&
+        error.getStatus() === HttpStatus.BAD_REQUEST
+      ) {
+        throw error;
+      }
+
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  public async updateUser(
+    updateUserDto: UpdateUserDto,
+    id: string,
+  ): Promise<void> {
+    try {
+      const { password, ...dtoWithoutPassword } = updateUserDto;
+      const user = await this.isUserExist(id);
+      if (!user) {
+        throw new HttpException(
+          EErrorMessage.USER_NOT_EXIST,
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      if (password) {
+        const salt = +this.configService.get('SALT');
+        const hashedPassword = await bcrypt.hash(updateUserDto.password, salt);
+        user.password = hashedPassword;
+      }
+      await this.userRepository.update(id, dtoWithoutPassword);
     } catch (error) {
       if (
         error instanceof HttpException &&
